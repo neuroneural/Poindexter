702b9af6a97b7d63c7e0c77b7d6c2b46998da12d
spikedoanz <manhdoanmainmail@gmail.com>
2023-09-15 14:09:42 -0400

    fixed out of context window prompts

--- poindexter_function.py
+++ poindexter_function.py
@@ -11,7 +11,6 @@ import urllib.request
 import requests
 import PyPDF2
 from io import BytesIO
-import random
 env_path = Path('.')/'.env'
 load_dotenv(dotenv_path=env_path)
 openai.api_key = token=os.environ['CHAT_TOKEN']
@@ -20,6 +19,18 @@ def get_html(url):
     response = urllib.request.urlopen(url)
     html_content = response.read().decode("utf-8")
     return html_content
+def get_pdf(url):
+    arxiv_id = url[22:]
+    url = "https://arxiv.org/pdf/" + arxiv_id + ".pdf"
+    response = requests.get(url)
+    # Ensure the request was successful
+    if response.status_code == 200:
+        # Step 2: Converting the PDF to a more readable format
+        with BytesIO(response.content) as open_pdf_file:
+            pdf = PyPDF2.PdfReader(open_pdf_file)
+            return pdf
+    else:
+        return("Pattern not found [Failed match flag]")
 def get_pdf_text(url):
     arxiv_id = url[22:]
     url = "https://arxiv.org/pdf/" + arxiv_id + ".pdf"
@@ -99,19 +110,6 @@ def fetch_title_from_html(url):
     else:
         return("Title not found [Failed match flag]")
     
-def dumb_this_down(text, age):
-    system_prompt = "Given the following text from a paper, summarize it so that a " + str(age) + " year old could read it"
-    response = openai.ChatCompletion.create(
-    model = "gpt-3.5-turbo-16k",
-    messages = [
-        {"role": "system", "content": system_prompt},
-        {"role": "user", "content": text},
-    ],
-    max_tokens = 100,
-    temperature = 0.8,
-    )
-    return response["choices"][0]["message"]["content"]
-
 
 ################################################
 ###########  FUNCTIONS FOR MATEO  ##############
@@ -134,9 +132,25 @@ def fetch_everything(url):
     content["abstract"] =  get_abstract(url)
     content["introduction"] = fetch_introduction(pdf)
     content["conclusion"] =  fetch_conclusion(pdf)
+    content["firstpage"] = "[Failed match flag]"
     return content
-test_url = "https://arxiv.org/abs/2205.03451"
-
+def fetch_everything_from_pdf(filepath):
+    with open(filepath, 'rb') as f:
+        pdf = PyPDF2.PdfReader(f)
+        num_pages = len(pdf.pages)
+        firstpage = pdf.pages[0].extract_text()
+        text = ""
+        for i in range(num_pages):
+            page = pdf.pages[i]
+            text += page.extract_text()
+    content = {}
+    content["title"] = ""
+    content["authors"] = [""]
+    content["abstract"] =  ""
+    content["introduction"] = fetch_introduction(text)
+    content["conclusion"] =  fetch_conclusion(text)
+    content["firstpage"] = firstpage
+    return content
 def append_title(meta_prompt, title):
     if ("[Failed match flag]" not in title):
         meta_prompt += "Summarize the following paper titled \"" + title + "\" "
@@ -183,6 +197,12 @@ def append_conclusion(meta_prompt, conclusion):
     conclusion = remove_newlinechar(conclusion)
     meta_prompt += "Conclusion: " + conclusion + "\n"
     return meta_prompt
+def append_firstpage(meta_prompt, firstpage):
+    if ("[Failed match flag]" in firstpage):
+        return meta_prompt
+    firstpage = remove_newlinechar(firstpage)
+    meta_prompt += "First page of paper: " + firstpage + "\n"
+    return meta_prompt
 def format_meta_prompt(content, level):
     meta_prompt = ""
     meta_prompt = append_title(meta_prompt, content["title"])
@@ -191,17 +211,29 @@ def format_meta_prompt(content, level):
     meta_prompt += "\n"
     meta_prompt = append_abstract(meta_prompt, content["abstract"])
     meta_prompt += "\n"
-    meta_prompt = append_introduction(meta_prompt, content["introduction"])
+    temp = append_introduction(meta_prompt, content["introduction"])
+    if len(temp) > 10000:
+        meta_prompt = temp
+        meta_prompt = append_conclusion(meta_prompt, content["conclusion"])
     meta_prompt += "\n"
     meta_prompt = append_conclusion(meta_prompt, content["conclusion"])
+    temp = append_firstpage(meta_prompt, content["firstpage"])
+    if len(temp) > 10000:
+        return meta_prompt
+    meta_prompt = append_firstpage(meta_prompt, content["firstpage"])
     return meta_prompt
 
 
 
 ##### Function for Mateoooooo #################
 # for level, select from "child", "teenager", "undegraduate", "graduate", "phd" 
-def poindexter(url, level = "child"):
-    content = fetch_everything(url)
+def poindexter(url = "", pdfpath = "", level = "child"):
+    if url != "":
+        content = fetch_everything(url)
+    elif filepath != "":
+        content = fetch_everything_from_pdf(pdfpath)
+        print("pdf detected")
+
     meta_prompt = format_meta_prompt(content, level)
     response = openai.ChatCompletion.create(
     model = "gpt-3.5-turbo-16k",
@@ -212,7 +244,15 @@ def poindexter(url, level = "child"):
     temperature = 0.8,
     )
     return response["choices"][0]["message"]["content"]
-print(poindexter(test_url, "child"))
+"""
+Levels = ["child", "teenager", "undergraduate", "graduate", "phd"]
+To call poindexter, needs either url or pdfpath
+poindexter(
+    url = string to url (optional), 
+    pdfpath = path to pdf (optional),
+    level = something from "Levels
+    
+"""
 
 def quirkyPoindexter():
     quirkyResponses = [
